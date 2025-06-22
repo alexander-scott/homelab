86152467fdd8ef461f054285e7f120a9cea86d93
Alexander Scott <alexanderscott@outlook.com>
2025-06-22 19:47:44 +0200

    Extend README

--- aws/__main__.py
+++ aws/__main__.py
@@ -1,7 +1,6 @@
 """An AWS Python Pulumi program"""
 
-from constants import AMI_ID
 from common import spin_up_ec2_instance
-
+from constants import AMI_ID
 
 spin_up_ec2_instance("m8g.large", "test", AMI_ID.UBUNTU_SERVER_2404)
