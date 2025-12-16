929e6ede9b544c29c1eb1146247d5c69f638cc3e
Alexander Scott <alexanderscott@outlook.com>
2025-12-16 20:14:31 +0100

    Rename file containing ec2 functions

--- aws/__main__.py
+++ aws/__main__.py
@@ -1,6 +1,6 @@
 """An AWS Python Pulumi program"""
 
-from common import spin_up_ec2_instance
 from constants import AMI_ID
+from ec2 import spin_up_ec2_instance
 
 spin_up_ec2_instance("m8g.xlarge", "test", AMI_ID.UBUNTU_SERVER_2404)
