88cf345613c2cc40b4e791466e501e18fae92976
Alexander Scott <alexanderscott@outlook.com>
2025-12-23 13:43:05 +0000

    Move pulumi projects into subdirs

--- aws/ec2/__main__.py
+++ aws/ec2/__main__.py
@@ -1,6 +1,10 @@
 """An AWS Python Pulumi program"""
 
+import sys
+
+sys.path.append("..")
+
 from constants import AMI_ID
-from ec2 import spin_up_ec2_instance
+from core import spin_up_ec2_instance
 
 spin_up_ec2_instance("m8g.xlarge", "test", AMI_ID.UBUNTU_SERVER_2404)
