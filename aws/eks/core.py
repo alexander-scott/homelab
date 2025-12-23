21d01cf4db0b5bcc8268d54c34742016a9a79a5f
Alexander Scott <alexanderscott@outlook.com>
2025-12-23 16:09:59 +0000

    Create deployment

--- aws/eks/core.py
+++ aws/eks/core.py
@@ -1,5 +1,6 @@
 import pulumi
 import pulumi_eks as eks
+import pulumi_kubernetes as k8s
 from constants import SUBNET_ID, SUBNET_ID_2, VPC_ID
 from pulumi_aws import ec2
 
@@ -32,4 +33,136 @@ def create_eks_cluster(project_name: str) -> None:
         ],
     )
 
+    # Might need to do:
+    # pulumi stack output kubeconfig > kubeconfig.yml
+    # export KUBECONFIG=./kubeconfig.yml
     pulumi.export("kubeconfig", cluster.kubeconfig)
+
+    test_ns = k8s.core.v1.Namespace(
+        "test-namespace",
+        pulumi.ResourceOptions(provider=cluster._provider),
+    )
+
+    # Use Helm to install the Nginx ingress controller
+    k8s.helm.v3.Release(
+        "ingresscontroller",
+        chart="nginx-ingress",
+        namespace=test_ns.metadata.name,
+        repository_opts={
+            "repo": "https://helm.nginx.com/stable",
+        },
+        skip_crds=True,
+        values={
+            "controller": {
+                "enableCustomResources": False,
+                "appprotect": {
+                    "enable": False,
+                },
+                "appprotectdos": {
+                    "enable": False,
+                },
+                "service": {
+                    "extraLabels": {
+                        "app": "nginx-ingress",
+                    },
+                },
+            },
+        },
+        version="0.14.1",
+    )
+
+    deployment = k8s.apps.v1.Deployment(
+        "whoami-deployment",
+        metadata=k8s.meta.v1.ObjectMetaArgs(
+            name="whoami-deployment",
+            namespace=test_ns.metadata.name,
+            labels={
+                "app": "whoami",
+            },
+        ),
+        spec=k8s.apps.v1.DeploymentSpecArgs(
+            replicas=1,
+            selector=k8s.meta.v1.LabelSelectorArgs(
+                match_labels={
+                    "app": "whoami",
+                },
+            ),
+            template=k8s.core.v1.PodTemplateSpecArgs(
+                metadata=k8s.meta.v1.ObjectMetaArgs(
+                    labels={
+                        "app": "whoami",
+                    },
+                ),
+                spec=k8s.core.v1.PodSpecArgs(
+                    containers=[
+                        k8s.core.v1.ContainerArgs(
+                            image="traefik/whoami",
+                            name="whoami",
+                            ports=[
+                                k8s.core.v1.ContainerPortArgs(
+                                    container_port=80,
+                                )
+                            ],
+                        )
+                    ],
+                ),
+            ),
+        ),
+    )
+
+    whoami_svc = k8s.core.v1.Service(
+        "whoami-svc",
+        metadata=k8s.meta.v1.ObjectMetaArgs(
+            name="whoami-svc",
+            namespace=test_ns.metadata.name,
+            labels={
+                "app": "whoami",
+            },
+        ),
+        spec=k8s.core.v1.ServiceSpecArgs(
+            ports=[
+                k8s.core.v1.ServicePortArgs(
+                    port=80,
+                    protocol="TCP",
+                    target_port=80,
+                )
+            ],
+            selector={
+                "app": deployment.pulumi_resource_name,
+            },
+        ),
+    )
+
+    k8s.networking.v1.Ingress(
+        "whoami-ingress",
+        metadata=k8s.meta.v1.ObjectMetaArgs(
+            name="whoami-ingress",
+            namespace=test_ns.metadata.name,
+            annotations={
+                "nginx.ingress.kubernetes.io/rewrite-target": "/",
+            },
+        ),
+        spec=k8s.networking.v1.IngressSpecArgs(
+            ingress_class_name="nginx-ingress",
+            rules=[
+                k8s.networking.v1.IngressRuleArgs(
+                    http=k8s.networking.v1.HTTPIngressRuleValueArgs(
+                        paths=[
+                            k8s.networking.v1.HTTPIngressPathArgs(
+                                backend=k8s.networking.v1.IngressBackendArgs(
+                                    service=k8s.networking.v1.IngressServiceBackendArgs(
+                                        name=whoami_svc.pulumi_resource_name,
+                                        port=k8s.networking.v1.ServiceBackendPortArgs(
+                                            number=80,
+                                        ),
+                                    ),
+                                ),
+                                path="/",
+                                path_type="Prefix",
+                            )
+                        ],
+                    ),
+                )
+            ],
+        ),
+    )
