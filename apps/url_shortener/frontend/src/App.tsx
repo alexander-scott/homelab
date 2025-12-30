7bdc15c59e5cd1a318eaa93b8c741b437ff0b2ec
Alexander Scott <alexanderscott@outlook.com>
2025-12-30 20:53:20 +0000

    Format files

--- apps/url_shortener/frontend/src/App.tsx
+++ apps/url_shortener/frontend/src/App.tsx
@@ -1,15 +1,15 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import { defaultSystem } from "@chakra-ui/react"
+import { ChakraProvider } from "@chakra-ui/react";
+import { defaultSystem } from "@chakra-ui/react";
 import Header from "./components/Header";
 import URLs from "./components/URLs";
 
 function App() {
-  return (
-    <ChakraProvider value={defaultSystem}>
-      <Header />
-      <URLs />
-    </ChakraProvider>
-  )
+	return (
+		<ChakraProvider value={defaultSystem}>
+			<Header />
+			<URLs />
+		</ChakraProvider>
+	);
 }
 
 export default App;
